c8f5732f5813fc2a8136f1f0cc8b1b8fdb75e4b6
Renzo Tan <renzotimtan@gmail.com>
2020-07-22 00:37:30 +0800

    Added grades page, added animations at home page, changed main color

---
+++ js/grades.js
@@ -0,0 +1,8 @@
+let termButtons = document.querySelectorAll(".add-grades button");
+
+for (let i = 0; i < termButtons.length; i++) {
+  termButtons[i].addEventListener("click", function () {
+    // If selected, don't do anything
+    // If not selected, make it the selected class
+  });
+}
