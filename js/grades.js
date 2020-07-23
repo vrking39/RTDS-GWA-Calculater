2aaa02207e51e7fe59a682f8dbb0f66b4b827f9e
Renzo Tan <renzotimtan@gmail.com>
2020-07-24 00:37:32 +0800

    Added calculator html/css

--- js/grades.js
+++ js/grades.js
@@ -2,7 +2,9 @@ let termButtons = document.querySelectorAll(".add-grades button");
 
 for (let i = 0; i < termButtons.length; i++) {
   termButtons[i].addEventListener("click", function () {
-    // If selected, don't do anything
-    // If not selected, make it the selected class
+    for (let i = 0; i < termButtons.length; i++) {
+      termButtons[i].classList.remove("button-selected");
+    }
+    this.classList.add("button-selected");
   });
 }
